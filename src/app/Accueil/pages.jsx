35b69da1edc03ba2d67b661e9678a5fddd327144
olisld <olivier.soulard4@gmail.com>
2025-09-10 14:52:25 +0200

    2nd commit

--- src/app/Accueil/pages.jsx
+++ src/app/Accueil/pages.jsx
@@ -1,3 +1,5 @@
+'use client'
+
 import {
   Card,
   CardAction,
@@ -8,21 +10,64 @@ import {
   CardTitle,
 } from "@/components/ui/card"
 
-export default function Accueil(){
-    return(
+import { useEffect, useState } from "react"
 
-        <Card>
-            <CardHeader>
-                <CardTitle>Card Title</CardTitle>
-                <CardDescription>Card Description</CardDescription>
-                <CardAction>Card Action</CardAction>
-            </CardHeader>
-            <CardContent>
-                <p>Card Content</p>
-            </CardContent>
-            <CardFooter>
-                <p>Card Footer</p>
-            </CardFooter>
+export default function Accueil(){
+    const tableau= [
+        {id:1,name:"olivier"},
+        {id:2,name:"MAthis"}
+    ]
+
+    const[isLoading,setIsLoading]=useState(false)
+    const[data,setData]=useState([])
+
+    async function fetchData(url){
+        try{
+            setIsLoading(true)
+            const response= await fetch(`${url}`)
+            const data= await response.json()
+            setData(data)
+            response.json
+            
+
+        }
+        catch{
+
+        }
+        finally{
+            setIsLoading(false)
+            console.log(data)
+        }
+        return data
+
+    }
+    useEffect(()=>{
+        fetchData("http://127.0.0.1:8000/api/questionnaires")
+    },[])
+    
+    return(
+        isLoading?(
+            <h1>chargement...</h1>
+        ):(
+            data.map((item,index)=>{
+            return(
+                <Card bgColor="bg-white" key={index}>
+                <CardHeader>
+                    <CardTitle>{item.title}</CardTitle>
+                    <CardDescription>Card Description</CardDescription>
+                    <CardAction>Card Action</CardAction>
+                </CardHeader>
+                <CardContent>
+                    <p>Card Content</p>
+                </CardContent>
+                <CardFooter>
+                    <p>Card Footer</p>
+                </CardFooter>
             </Card>
+            )
+            
+        })
+            
+        )
     )
 }
--- src/components/ui/card.jsx
+++ src/components/ui/card.jsx
@@ -4,13 +4,14 @@ import { cn } from "@/lib/utils"
 
 function Card({
   className,
+  bgColor,
   ...props
 }) {
   return (
     <div
       data-slot="card"
       className={cn(
-        "bg-card text-card-foreground flex flex-col gap-6 rounded-xl border py-6 shadow-sm",
+        "bg-card text-card-foreground flex flex-col gap-6 rounded-xl border py-6 shadow-sm",bgColor,
         className
       )}
       {...props} />
